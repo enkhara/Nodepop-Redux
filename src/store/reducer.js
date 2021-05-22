5ae626da8e946496cedfa3e9a217cb9689864b11
Georgina Torralba <georgina.torralba@gmail.com>
2021-05-22 22:17:42 +0200

    created reducer login/logout

---
+++ src/store/reducer.js
@@ -0,0 +1,20 @@
+import { authLogin, authLogout } from './actions';
+import { AUTH_LOGIN, AUTH_LOGOUT } from './types';
+
+const initialState = {
+	auth: false,
+	//adverts: [],
+};
+
+function reducer(state = initialState, action) {
+	switch (action.type) {
+		case AUTH_LOGIN:
+			return { ...state, auth: true };
+		case AUTH_LOGOUT:
+			return { ...state, auth: false };
+		default:
+			return state;
+	}
+}
+
+export default reducer;
